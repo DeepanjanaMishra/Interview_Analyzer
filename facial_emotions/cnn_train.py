ed3abc675694f4582babfc7102de0ad2ea93a879
Deepanjana Mishra <deep2005mis@gmail.com>
2026-06-24 20:48:49 +0530

    cnn model training for facial emotion detection

---
+++ facial_emotions/cnn_train.py
@@ -0,0 +1,202 @@
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+import random
+import numpy as np
+
+torch.manual_seed(42)
+np.random.seed(42)
+random.seed(42)
+
+# Path to your dataset
+data_dir = r"C:\Users\Deepanjana Mishra\OneDrive - BENNETT UNIVERSITY\Desktop\AI_ML_Project\Facial emotions"
+
+# Image transforms
+train_transform = transforms.Compose([
+    transforms.Grayscale(num_output_channels=1),
+    transforms.Resize((48, 48)),
+
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(10),
+    transforms.RandomAffine(
+        degrees=0,
+        translate=(0.1, 0.1),
+        scale=(0.9, 1.1)
+    ),
+
+    transforms.ColorJitter(brightness=0.2, contrast=0.2),
+
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+
+test_transform = transforms.Compose([
+    transforms.Grayscale(num_output_channels=1),
+    transforms.Resize((48, 48)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+
+# Load dataset
+train_data = datasets.ImageFolder(root=f"{data_dir}/train", transform=train_transform)
+test_data = datasets.ImageFolder(root=f"{data_dir}/test", transform=test_transform)
+
+train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=32, shuffle=False)
+
+print("Number of training samples:", len(train_data))
+print("Number of test samples:", len(test_data))
+print("Classes:", train_data.classes)
+
+
+
+# Test one batch
+images, labels = next(iter(train_loader))
+print("Batch image shape:", images.shape)
+print("Batch labels shape:", labels.shape)
+
+import torch.nn as nn
+import torch.optim as optim
+
+class EmotionCNN(nn.Module):
+    def __init__(self):
+        super(EmotionCNN, self).__init__()
+
+        self.conv_layers = nn.Sequential(
+
+    # Block 1
+    nn.Conv2d(1, 32, kernel_size=3, padding=1),
+    nn.BatchNorm2d(32),
+    nn.ReLU(),
+
+    nn.Conv2d(32, 32, kernel_size=3, padding=1),
+    nn.BatchNorm2d(32),
+    nn.ReLU(),
+
+    nn.MaxPool2d(2),
+
+    # Block 2
+    nn.Conv2d(32, 64, kernel_size=3, padding=1),
+    nn.BatchNorm2d(64),
+    nn.ReLU(),
+
+    nn.Conv2d(64, 64, kernel_size=3, padding=1),
+    nn.BatchNorm2d(64),
+    nn.ReLU(),
+
+    nn.MaxPool2d(2),
+
+    # Block 3
+    nn.Conv2d(64, 128, kernel_size=3, padding=1),
+    nn.BatchNorm2d(128),
+    nn.ReLU(),
+
+    nn.Conv2d(128, 128, kernel_size=3, padding=1),
+    nn.BatchNorm2d(128),
+    nn.ReLU(),
+
+    nn.MaxPool2d(2)
+)
+
+        self.flatten = nn.Flatten()
+
+        self.feature_layer = nn.Sequential(
+            nn.Linear(128 * 6 * 6, 1024),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+
+            nn.Linear(1024, 256),
+            nn.ReLU(),
+            nn.Dropout(0.5)
+)
+
+        self.classifier = nn.Linear(256, 7)
+
+    def forward(self, x):
+        x = self.conv_layers(x)
+        x = self.flatten(x)
+        features = self.feature_layer(x)
+        output = self.classifier(features)
+        return output, features
+    
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+else:
+    device = torch.device("cpu")
+
+print("Using device:", device)
+
+model = EmotionCNN().to(device)
+
+from collections import Counter
+
+counts = Counter(train_data.targets)
+total = sum(counts.values())
+
+class_weights = [total / counts[i] for i in range(len(counts))]
+class_weights = torch.tensor(class_weights, dtype=torch.float).to(device)
+
+criterion = nn.CrossEntropyLoss()
+
+optimizer = optim.Adam(model.parameters(), lr=0.0005)
+
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.5)
+
+epochs = 30
+
+for epoch in range(epochs):
+    model.train()
+    running_loss = 0.0
+    correct = 0
+    total = 0
+
+    for images, labels in train_loader:
+        images, labels = images.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+
+        outputs, _ = model(images)
+        loss = criterion(outputs, labels)
+
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+        _, predicted = torch.max(outputs, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+    train_acc = 100 * correct / total
+
+    print(f"Epoch [{epoch+1}/{epochs}] "
+          f"Loss: {running_loss:.4f} "
+          f"Train Accuracy: {train_acc:.2f}%")
+    scheduler.step()
+
+# ----------------------
+# Evaluate on Test Data
+# ----------------------
+model.eval()
+
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for images, labels in test_loader:
+        images, labels = images.to(device), labels.to(device)
+
+        outputs, _ = model(images)
+        _, predicted = torch.max(outputs, 1)
+
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+test_acc = 100 * correct / total
+print(f"\nTest Accuracy: {test_acc:.2f}%")
+
+torch.save(model.state_dict(), "emotion_cnn.pth")
+print("Model saved successfully!")
